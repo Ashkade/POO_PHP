c345934aa87565b693232a770e0f298da4c37a71
Ashkade <betari.laid@gmail.com>
2023-10-31 16:38:43 +0100

    Debut découverte class php

---
+++ centre/centre_index.php
@@ -0,0 +1,46 @@
+<div class="content">
+    <?php
+
+        class Personne{ // création d'une classe "Personne"
+            public $prenom; // création d'une variable avec des accès publics
+            public $nom;
+            public $age;
+
+            public function __construct($prenom, $nom, $age){
+                $this->prenom=$prenom;
+                $this->nom=$nom;
+                $this->age=$age;
+            }
+
+            public function nomComplet(){
+                // return $this->nom.' '.$this->prenom.' '.$this->age.PHP_EOL;
+                return sprintf('%s %s %d',$this->nom,$this->prenom,$this->age);
+            }
+
+            public function danser(){
+                echo $this->prenom." est entrain de danser".PHP_EOL;
+            }
+        }
+
+        
+
+
+        //création d'un nouvel objet à partir de la classe Personne ou nouvelle instance de la classe personnne
+        $magne = new Personne('Magne', 'Matangou', 43);
+        $isaac = new Personne('Isaac', 'Lbôgoss', 24);
+        
+        echo $magne->prenom . PHP_EOL;
+        echo $magne->nom . PHP_EOL;
+        echo $magne->age . PHP_EOL;
+        $magne-> danser();
+        $toutlenom=$magne-> nomComplet();
+        echo $toutlenom;
+        // echo $magne ->prenom . PHP_EOL;
+
+        // echo $isaac ->prenom . PHP_EOL;
+
+
+
+
+    ?>
+    </div>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" rel="stylesheet">
+    <link href="style/style.css" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body>
+    
+    <?php
+        include_once('header.php');
+        include_once('centre/centre_index.php');
+        include_once('footer.php');
+    ?>
+
+</body>
+</html>
