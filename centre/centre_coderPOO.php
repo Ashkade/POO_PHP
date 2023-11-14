ea582145e16c09f5721f2c686e45f4eadfecae1e
Ashkade <betari.laid@gmail.com>
2023-11-14 16:44:31 +0100

    modification coderPOO

---
+++ centre/centre_coderPOO.php
@@ -0,0 +1,51 @@
+<div class="content">
+
+    <?php
+
+        /*
+        CODER EN ORIENTÉE OBJET INTRODUCTION
+
+            La programmation orientée objet est beaucoup moins linéaire par rapport à la programmation procédurale. Son principal avantage est d'être mieux organisée permettant ainsi de facilité la lisibilité du code et sa maintenance.
+            Son concept est un peu plus difficile à maîtriser que celui du paradygme procédural.
+            Son principe est de faire en sorte que tout ce que nous allons coder devienne un objet.
+
+            Qu'est ce qu'un objet ?
+            Un objet est tout ce que vous avez l'habitude de manipuler au quotidien par exemple : un livre, un fauteuil...
+
+            Différence entre procédural et POO
+            Ce sont deux manières de programmer. Sur le fond, ils utilisent le même langage, mais leur différence se situe au niveau de l'approche conceptuel du programme.
+            Comme nous l'avons compris le POO a l'avantage d'avoir un code plus clair que celui procédural et donc plus facile à mettre à jour. Il sera également beaucoup plus facile de la réutiliser dans d'autres portions de code de notre application ou site internet ce qui est loin d'être négligeable.
+
+            En résumé, on peut retenir qu'il existe deux façon de programmer en PHP : POO ou procédural.
+            Il n'y a donc pas de bonnes ou de mauvaises manières de coder. Nous disont simplement que plus votre site ou application grossira, plus vous aurez intérêt à utiliser la POO.
+
+
+        LES FONDAMENTAUX DE LA POO
+
+            Principe de la POO
+
+            La POO se base sur le principe que tout élément inscrit est un objet. Le principe sera de créer des objets, pour ce faire, nous utilisons des classes.
+            Une classe est donc un bloc de code culturel qui servara de moule pour la création d'un ou plusieurs objets.
+            
+            La classe
+            Nous venons de voir que le principe d'une classe est de créer un objet. La classe utilise le mot clef class et contient un ensemble de fonctions qui servira de modèle à la création d'un objet.
+            Ses fonctions porteront le nom de "méthodes". Lorsqu'on créé une fonction à l'intérieur d'une classe, on l'appelle "méthode". En revanche le principe de fonctionnement est totalement identique.
+
+            Les objets
+            Maintenant que nous avons défini une classe, nous pouvons alors créer un objet issu de cette classe. On dit alors qu'on instancie la classe ou que l'objet est une instance de la classe.
+            Ces objets seront constuits à partir des méthodes (fonctions) et des propriétés ou attributs (variables) de la classe.
+
+            En résumé, on peut retenir que la POO est basée sur les objets et les classes. On créé des objets à partir d'une classe. Chaque objet a accès aux méthodes et aux propriétés de la classe qui les a créées.
+
+            Création d'une classe
+            Par convention, le nom de la classe commence par une lettre majuscule et chaque classe sera créée dans un fichier unique.
+            Le fichier PHP contiendra uniquement la classe ainsi que ses attributs et ses méthodes. Ce fichier pourra alors être appeler partout dans notre site/application grâce aux inclusions de fichiers.
+            */
+            
+
+
+
+
+    ?>
+
+</div>
